2d804dbafee715aac37193438008e76d45f78f0b
TommyPerfect <tnpstylist@gmail.com>
2024-12-14 00:08:49 +0100

    cleanup

--- compress.py
+++ compress.py
@@ -1,94 +1,28 @@
-import os, ffmpeg, time, sys, re
-from better_ffmpeg_progress import FfmpegProcess
+import os, ffmpeg, sys, re,  video
 
 cmd = ['ffmpeg', '-hwaccel','cuda', '-loglevel','quiet', '-stats']
-
-
-def compress_video(video_full_path, output_file_name, target_size):
-    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
-    min_audio_bitrate = 32000
-    max_audio_bitrate = 256000
-
-    probe = ffmpeg.probe(video_full_path)
-    # Video duration, in s.
-    duration = min(float(probe['format']['duration']), 599.0)
-    # Audio bitrate, in bps.
-    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-    # Target total bitrate, in bps.
-    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
-
-    # Target audio bitrate, in bps
-    if 10 * audio_bitrate > target_total_bitrate:
-        audio_bitrate = target_total_bitrate / 10
-        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-            audio_bitrate = min_audio_bitrate
-        elif audio_bitrate > max_audio_bitrate:
-            audio_bitrate = max_audio_bitrate
-    # Target video bitrate, in bps.
-    video_bitrate = target_total_bitrate - audio_bitrate
-    
-    #os.system('ffmpeg -i ' + video_full_path + ' -vcodec libvpx -acodec libvorbis ' + output_file_name)
-
-    i = ffmpeg.input(video_full_path)
-    #ffmpeg.output(i, os.devnull,
-    #              **{'c:v': 'libx265', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
-    #              ).overwrite_output().run()
-    
-    # das ding tuts
-    ffmpeg.output(i, output_file_name,
-        **{
-            'c:v': 'libx265',
-            'b:v': video_bitrate,
-            'pass': 20,
-            'c:a': 'aac',
-            'b:a': audio_bitrate,
-            't': '00:09:59',
-        }
-    ).overwrite_output().run(cmd)
-                 
-                 
-#does it with nice status bar and percentage, but uses 10% cpu for it...
-# process = FfmpegProcess([
-    # "ffmpeg", "-y",
-    # "-hwaccel", "cuda",
-    # "-i", video_full_path, 
-    # "-c:v", "libx265", 
-    # "-b:v", str(video_bitrate), 
-    # "-pass", str(20), 
-    # "-c:a", "aac", 
-    # "-b:a", str(audio_bitrate), 
-    # "-t", "00:09:59", 
-    # "-stats_period", "10",
-    # output_file_name
-# ])    
-# # Use the run method to run the FFmpeg command.
-# process.run(
-    # progress_bar_description = "Converting to " + output_file_name
-# )
-    
-#, 'v:f': 'scale=540:-1'               -hwaccel cuda
-#    os.system('ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i '.video_full_path.' -c:v h264_nvenc -preset slow '.output_file_name)
-    
-wished_size = 50
+wishedSize = 50
+passes = 20
 reduceiftoobig = 5
-c = 0
+maxTime = 599.0
+
 
 # für drag und drop und so
 for param in sys.argv[1:]:
     droppedFile = param
     #print(droppedFile)
-    size = wished_size
+    size = wishedSize
+    c = 0
     while True:
         newFile = re.sub('\\..*?$', '_small_' + str(int(size)) + '.mp4', droppedFile)
         print(newFile)
-        #time.sleep(1)
-        compress_video(droppedFile, newFile, size * 1000)
-        newsize = os.stat(newFile).st_size / (1024 * 1024)
-        if newsize < wished_size:
+        video.compress(droppedFile, newFile, size * 1000, maxTime, passes, cmd)
+        newSize = os.stat(newFile).st_size / (1024 * 1024)
+        if newSize < wishedSize:
             break
         else:
-            size *= (wished_size/newsize)
+            size *= (wishedSize/newSize)
             size -= (c * reduceiftoobig)
             c = c + 1
-            print('File to big, doing again with ' + str(size) + ' wishedsize cause ' + str(newsize) + ' > ' + str(wished_size) + ' !!!!!!')
-    
+            print('File to big, doing again with ' + str(size) + ' wishedsize cause ' + str(newSize) + ' > ' + str(wishedSize) + ' !!!!!!')
+ 
---
+++ video.py
@@ -0,0 +1,39 @@
+import ffmpeg, time
+
+#gets Video- and Audio Bitrate of Video of desired Size
+def getVideoAudioBitrate(path, targetSize, maxTime):
+    minAudioBitrate = 32000
+    maxAudioBitrate = 256000
+    probe = ffmpeg.probe(path)
+    # Video duration, in s.
+    duration = min(float(probe['format']['duration']), maxTime)
+    # Audio bitrate, in bps.
+    audioBitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    # Target total bitrate, in bps.
+    targetTotalBitrate = (targetSize * 1024 * 8) / (1.073741824 * duration)
+    # Target audio bitrate, in bps
+    if 10 * audioBitrate > targetTotalBitrate:
+        audioBitrate = targetTotalBitrate / 10
+        if audioBitrate < minAudioBitrate < targetTotalBitrate:
+            audioBitrate = minAudioBitrate
+        elif audioBitrate > maxAudioBitrate:
+            audioBitrate = maxAudioBitrate
+    # Target video bitrate, in bps.
+    videoBitrate = targetTotalBitrate - audioBitrate
+    return videoBitrate, audioBitrate
+
+# compresses a video
+def compress(videoPath, outputFileName, targetSize, maxTime, passes, cmd):
+    maxTimeStr = time.strftime('%H:%M:%S', time.gmtime(maxTime))
+    videoBitrate, audioBitrate = getVideoAudioBitrate(videoPath, targetSize, maxTime)
+    i = ffmpeg.input(videoPath)
+    ffmpeg.output(i, outputFileName,
+        **{
+            'c:v': 'libx265',
+            'b:v': videoBitrate,
+            'pass': passes,
+            'c:a': 'aac',
+            'b:a': audioBitrate,
+            't': maxTimeStr,
+        }
+    ).overwrite_output().run(cmd)
