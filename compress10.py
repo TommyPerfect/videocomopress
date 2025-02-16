76ef4343a0d5711ed96b94741644baa42ae02031
TommyPerfect <tnpstylist@gmail.com>
2025-02-16 06:17:25 +0100

    the comment script updated

--- compress10.py
+++ compress10.py
@@ -1,28 +1,7 @@
-import os, ffmpeg, sys, re, video
+import videoCommand
 
-cmd = ['ffmpeg', '-hwaccel','cuda', '-loglevel','quiet', '-stats']
-wishedSize = 10
-passes = 20
-reduceiftoobig = 5
-maxTime = 599.0
+sizeWished = 10
+timeMax = 10.0
 
 
-# für drag und drop und so
-for param in sys.argv[1:]:
-    droppedFile = param
-    #print(droppedFile)
-    size = wishedSize
-    c = 0
-    while True:
-        newFile = re.sub('\\..*?$', '_small_' + str(int(size)) + '.mp4', droppedFile)
-        print(newFile)
-        video.compress(droppedFile, newFile, size * 1000, maxTime, passes, cmd)
-        newSize = os.stat(newFile).st_size / (1024 * 1024)
-        if newSize < wishedSize:
-            break
-        else:
-            size *= (wishedSize/newSize)
-            size -= (c * reduceiftoobig)
-            c = c + 1
-            print('File to big, doing again with ' + str(size) + ' wishedsize cause ' + str(newSize) + ' > ' + str(wishedSize) + ' !!!!!!')
- 
+videoCommand.doDropped(sizeWished, timeMax)
